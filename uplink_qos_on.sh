ebcc207fb3e3df2afb7d94bdb1eef7946b2c33df
Freifunk Cottbus <noc@freifunk-cottbus.de>
2016-03-17 23:43:17 +0100

    switched language

--- uplink_qos_on.sh
+++ uplink_qos_on.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-uplinkstatus=$(uci get qos.wan.enabled 2>/dev/null)
+uplinkstate=$(uci get qos.wan.enabled 2>/dev/null)
 
-if [ "$uplinkstatus" = "0" ]
+if [ "$uplinkstate" = "0" ]
   then
     uci set qos.wan.enabled=1 2> /dev/null && \
     uci commit qos 2> /dev/null && \
