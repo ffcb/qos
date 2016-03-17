9676b6c4cf95911d9ce3ce6cf130786b5439a4e6
Freifunk Cottbus <noc@freifunk-cottbus.de>
2016-03-17 19:11:47 +0100

    Update uplink_qos_off.sh

--- uplink_qos_off.sh
+++ uplink_qos_off.sh
@@ -3,9 +3,9 @@
 uplinkstatus=$(uci get qos.wan.enabled 2>/dev/null)
 
 if [ "$uplinkstatus" = "1" ]; then
-
-uci set qos.wan.enabled=0 2> /dev/null && \
-uci commit qos 2> /dev/null && \
-/etc/init.d/qos reload 2> /dev/null
+  then
+    uci set qos.wan.enabled=0 2> /dev/null && \
+    uci commit qos 2> /dev/null && \
+    /etc/init.d/qos reload 2> /dev/null
 
 fi
